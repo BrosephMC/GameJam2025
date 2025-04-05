ceef793e14823f0728e8d63b9e413f9c69f5ea54
Everest877 <bearyage@gmail.com>
2025-04-05 12:56:33 -0500

    new file for spec objs

---
+++ SpecialObjs.js
@@ -0,0 +1,20 @@
+//function to place noninteractible objects
+function makeSpecObj(){
+
+    // BABYLON.SceneLoader.ImportMesh("", "scenes/", "skull.babylon", scene, function (newMeshes) {
+    BABYLON.SceneLoader.ImportMesh("", "CoffeeCart/", "CoffeeCart_01_1k.gltf", scene, function (newMeshes) {
+        // Set the target of the camera to the first imported mesh
+        newMeshes[0].position.y = 5;
+    });
+
+    BABYLON.SceneLoader.ImportMesh("", "Chair/", "plastic_monobloc_chair_01_1k.gltf", scene, function (newMeshes) {
+        // Set the target of the camera to the first imported mesh
+        newMeshes[0].position.y = 10;
+    });
+
+    BABYLON.SceneLoader.ImportMesh("", "Toilet/", "uploads_files_2406324_Toilet.gltf", scene, function (newMeshes) {
+        // Set the target of the camera to the first imported mesh
+        newMeshes[0].position.y = 15;
+    });
+
+}
